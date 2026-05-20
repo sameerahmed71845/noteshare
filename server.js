de5686f244c48a992c73f038829a4cb38ff81aff
Sameer Ahmad <sameerahmadsyed04@gmail.com>
2026-05-20 14:04:26 +0530

    Fixed upload issue and notes update

--- server.js
+++ server.js
@@ -6,104 +6,273 @@ const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
 
+const path = require("path");
+
 const Note = require("./models/note");
 
 const app = express();
-const path = require("path");
 
-app.use(express.static(path.join(__dirname, "frontend")));
-app.get("/", (req, res) => {
- res.sendFile(path.join(__dirname, "frontend", "index.html"));
-});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ================= CLOUDINARY CONFIG =================
+// ================= FRONTEND =================
+
+app.use(express.static(path.join(__dirname,"frontend")));
+
+app.get("/",(req,res)=>{
+
+res.sendFile(
+path.join(__dirname,"frontend","index.html")
+
+);
+
+});
+
+// ================= CLOUDINARY =================
+
 cloudinary.config({
-  cloud_name: "ds7gxnp5p",
-  api_key: "455578232828944",
-  api_secret: "aOGQPAuBMcN5NT-rCCOIyC4s0ic"
+
+cloud_name:"ds7gxnp5p",
+
+api_key:"455578232828944",
+
+api_secret:"aOGQPAuBMcN5NT-rCCOIyC4s0ic"
+
 });
 
-// ================= STORAGE =================
+// ================= CLOUD STORAGE =================
+
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "notes",
-    resource_type: "raw" // for PDF
-  }
+
+cloudinary,
+
+params:{
+
+folder:"notes",
+
+resource_type:"raw"
+
+}
+
 });
 
-const upload = multer({ storage });
+const upload = multer({
+
+storage
+
+});
 
 // ================= MONGODB =================
-mongoose.connect("mongodb://admin:sameer71845@ac-zjvhrzl-shard-00-00.szbepmy.mongodb.net:27017,ac-zjvhrzl-shard-00-01.szbepmy.mongodb.net:27017,ac-zjvhrzl-shard-00-02.szbepmy.mongodb.net:27017/?ssl=true&replicaSet=atlas-zhiir5-shard-0&authSource=admin&appName=noteshare")
-  .then(() => console.log("MongoDB Connected ✅"))
-  .catch(err => console.log("DB ERROR ❌", err));
-// ================= ROUTES =================
 
-// GET NOTES
-app.get("/notes", async (req, res) => {
-  const notes = await Note.find();
-  res.json(notes);
+mongoose.connect(
+
+"mongodb://admin:sameer71845@ac-zjvhrzl-shard-00-00.szbepmy.mongodb.net:27017,ac-zjvhrzl-shard-00-01.szbepmy.mongodb.net:27017,ac-zjvhrzl-shard-00-02.szbepmy.mongodb.net:27017/?ssl=true&replicaSet=atlas-zhiir5-shard-0&authSource=admin&appName=noteshares"
+
+)
+
+.then(()=>{
+
+console.log("MongoDB Connected ✅");
+
+})
+
+.catch((err)=>{
+
+console.log("DB ERROR ❌",err);
+
 });
 
-// UPLOAD NOTE
-app.post("/upload-note", upload.single("pdf"), async (req, res) => {
- try {
+// ================= GET NOTES =================
 
-   if (!req.file) {
-     return res.status(400).json({
-       message: "No PDF uploaded"
-     });
-   }
+app.get("/notes",async(req,res)=>{
 
-   const { title, subject } = req.body;
+try{
 
-   const newNote = new Note({
-     title,
-     subject,
-     pdfUrl: req.file.path,
-     fileName: req.file.originalname
-   });
+const notes = await Note.find();
 
-   await newNote.save();
+res.json(notes);
 
-   res.json({
-     message: "Uploaded Successfully ✅"
-   });
-
- } catch (err) {
-
-   console.log(err); // ADD THIS
-
-   res.status(500).json({
-     error: err.message
-   });
-
- }
 }
-// DELETE NOTE
-app.delete("/delete-note/:id", async (req, res) => {
-  await Note.findByIdAndDelete(req.params.id);
-  res.json({ message: "Deleted ✅" });
+
+catch(err){
+
+res.status(500).json(err);
+
+}
+
 });
 
-app.post("/download/:id", async (req, res) => {
-  await Note.findByIdAndUpdate(req.params.id, {
-    $inc: { downloads: 1 }
-  });
-  res.send("Updated");
+// ================= UPLOAD NOTE =================
+
+app.post(
+
+"/upload-note",
+
+upload.single("pdf"),
+
+async(req,res)=>{
+
+try{
+
+console.log(req.body);
+
+console.log(req.file);
+
+if(!req.file){
+
+return res.status(400).json({
+
+message:"No PDF Uploaded"
+
 });
 
-app.get("/trending", async (req, res) => {
-  const notes = await Note.find().sort({ downloads: -1 }).limit(5);
-  res.json(notes);
+}
+
+const {title,subject}=req.body;
+
+const newNote = new Note({
+
+title,
+
+subject,
+
+pdfUrl:req.file.path,
+
+fileName:req.file.originalname
+
 });
 
+await newNote.save();
+
+res.json({
+
+message:"Uploaded Successfully ✅"
+
+});
+
+}
+
+catch(err){
+
+console.log(err);
+
+res.status(500).json({
+
+error:err.message
+
+});
+
+}
+
+}
+
+);
+
+// ================= DELETE NOTE =================
+
+app.delete(
+
+"/delete-note/:id",
+
+async(req,res)=>{
+
+try{
+
+await Note.findByIdAndDelete(
+
+req.params.id
+
+);
+
+res.json({
+
+message:"Deleted ✅"
+
+});
+
+}
+
+catch(err){
+
+res.status(500).json(err);
+
+}
+
+}
+
+);
+
+// ================= DOWNLOAD COUNT =================
+
+app.post(
+
+"/download/:id",
+
+async(req,res)=>{
+
+await Note.findByIdAndUpdate(
+
+req.params.id,
+
+{
+
+$inc:{
+
+downloads:1
+
+}
+
+}
+
+);
+
+res.send("Updated");
+
+}
+
+);
+
+// ================= TRENDING =================
+
+app.get(
+
+"/trending",
+
+async(req,res)=>{
+
+const notes=
+
+await Note.find()
+
+.sort({
+
+downloads:-1
+
+})
+
+.limit(5);
+
+res.json(notes);
+
+}
+
+);
 
 // ================= SERVER =================
-app.listen(3000, () => {
-  console.log("Server running on port 3000 🚀");
+
+const PORT=
+
+process.env.PORT||
+
+3000;
+
+app.listen(PORT,()=>{
+
+console.log(
+
+`Server running on port ${PORT} 🚀`
+
+);
+
 });
